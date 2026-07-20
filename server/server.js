21b0de5129cd111bd5c65d2481d33d7de43347d7
nana <nananurudeen86@gmail.com>
2026-07-20 13:58:58 +0100

    fix: allow vercel cors

--- server/server.js
+++ server/server.js
@@ -15,10 +15,16 @@ const app = express()
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://devto-clone-l70omnhvc-nana-devto.vercel.app'
+]
+
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: allowedOrigins,
   credentials: true
 }))
+
 app.use(express.json())
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
